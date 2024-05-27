6f94b408741568bc77e797e9f4928125e76ab602
Alexander Scarlett <ascarlett92@yahoo.com>
2024-05-26 22:36:48 -0400

    Updated employee first and last names, roles and ids

--- db/seeds.sql
+++ db/seeds.sql
@@ -15,15 +15,24 @@ VALUES
     ('Sales Person', 1,80000),
     ('Lead Engineer', 2,150000);
     ('Software Engineer', 2, 120000);
-    ('Accoount Manager', 3, 125000);
-    ('');
-    ('');
-    ('');
+    ('Account Manager', 3, 160000);
+    ('Accountant', 3, 125000);
+    ('Legal Team Lead', 4, 250000);
+    ('Lawyer', 4, 190000);
 
 INSERT INTO employee(first_name, last_name,role_id)
 VALUES ('John','Doe',1),
     ('Mike','Chan',2);
+    ('Ashley','Rodriquez',3);
+    ('kevin','Tupik',4);
+    ('Kunal','Singh',5);
+    ('Malia','Brown',6);
+    ('Sarah','Lourd' ,7);
+    ('Tom','Allen',8);
 
 
     UPDATE employee set manager_id=1 where id=2;
+    UPDATE employee set manager_id=3 where id=4;
+    UPDATE employee set manager_id=5 where id=6;
+    UPDATE employee set manager_id=7 where id=8;
     --  DO THE REST OF THE EMPLOYEE NAMES and Engineeringre add legal and finance
